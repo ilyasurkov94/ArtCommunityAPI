8782c4e8dce2921a9dc328870bd84f64e9c9402f
ilyasurkov94 <ilyasurkov1994@mail.ru>
2021-10-21 16:27:11 +0300

    На ревью 3

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -5,8 +5,8 @@ from posts.models import Post, Group
 from .serializers import PostSerializer, GroupSerializer
 from .serializers import FollowSerializer, CommentSerializer
 from rest_framework.permissions import AllowAny
-from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
 
 
 class PostViewSet(viewsets.ModelViewSet):
